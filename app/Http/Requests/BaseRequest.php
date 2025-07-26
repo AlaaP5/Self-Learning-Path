d5f44734c39b4b41a02558bd44680328cd106305
AlaaP5 <alaaalshareef3012002@gmail.com>
2025-07-26 15:53:16 +0300

    Add BaseRequest File

---
+++ app/Http/Requests/BaseRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Responses\ApiResponse;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class BaseRequest extends FormRequest
+{
+    /**
+     * Define dynamic rules.
+     */
+    protected array $rules = [];
+
+    /**
+     * Define dynamic messages.
+     */
+    protected array $messages = [];
+
+    /**
+     * Set the validation rules dynamically.
+     */
+    public function rules(): array
+    {
+        return $this->rules;
+    }
+
+    /**
+     * Set the validation messages dynamically.
+     */
+    public function messages(): array
+    {
+        return array_merge(trans('validation'), $this->messages);
+    }
+
+    /**
+     * Automatically merge authenticated user_id into request.
+     */
+    protected function prepareForValidation(){}
+
+
+    /**
+     * Allow authorization dynamically.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+    /**
+     * Override failedValidation to return only the first error message.
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors()->first();
+        $response = ApiResponse::error($errors)->toJsonResponse();
+        throw new HttpResponseException($response);
+    }
+}
