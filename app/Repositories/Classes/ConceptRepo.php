f741abdc0419528d9e3c8c9e0ebfda55666102c1
AlaaP5 <alaaalshareef3012002@gmail.com>
2025-08-14 12:39:18 +0300

    Add concept classes and interfaces to establish core domain models

---
+++ app/Repositories/Classes/ConceptRepo.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Repositories\Classes;
+
+use App\Models\Concept;
+use App\Models\ConceptPrerequisite;
+use App\Repositories\Interfaces\ConceptRepoInterface;
+use Illuminate\Database\Eloquent\Collection;
+
+class ConceptRepo implements ConceptRepoInterface
+{
+    public function __construct(protected Concept $conceptModel, protected ConceptPrerequisite $conceptPrerequisiteModel) {}
+
+    function findByName(string $name): Concept
+    {
+        return $this->conceptModel->where('name', $name)->first();
+    }
+
+    function getAll(): Collection
+    {
+        return $this->conceptModel->all();
+    }
+
+    public function getPrerequisitesForConcepts(array $conceptIds): array
+    {
+        return $this->conceptPrerequisiteModel->whereIn('concept_id', $conceptIds)
+            ->get()
+            ->map(function ($item) {
+                return [
+                    'concept_id' => $item->concept_id,
+                    'requires' => $item->prerequisite_concept_id
+                ];
+            })
+            ->toArray();
+    }
+
+    public function getConceptsWithResources(array $conceptIds)
+    {
+        return $this->conceptModel->whereIn('id', $conceptIds)
+            ->with('resources')
+            ->get()
+            ->keyBy('id');
+    }
+}
