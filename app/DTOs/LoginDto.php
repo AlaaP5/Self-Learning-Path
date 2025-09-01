4285996734fec584f91223b2c0fa1724b9079fdf
AlaaP5 <alaaalshareef3012002@gmail.com>
2025-09-02 02:09:18 +0300

    add loginDto class

---
+++ app/DTOs/LoginDto.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DTOs;
+
+use App\Models\User;
+
+class LoginDto
+{
+
+    public int $id;
+    public string $email;
+    public string $token;
+
+    public function __construct(?User $user ,string $token)
+    {
+        $this->id=$user->id;
+        $this->email=$user->email;
+        $this->token=$token;
+    }
+
+
+
+}
