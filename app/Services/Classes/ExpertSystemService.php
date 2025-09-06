5881afd83f1ad6dda64a76d7ccf979d4780f40c2
AlaaP5 <alaaalshareef3012002@gmail.com>
2025-09-06 14:04:18 +0300

    add getRequires function for job

--- app/Services/Classes/ExpertSystemService.php
+++ app/Services/Classes/ExpertSystemService.php
@@ -3,13 +3,18 @@
 // app/Services/Classes/ExpertSystemService.php
 namespace App\Services\Classes;
 
+use App\Repositories\Interfaces\ConceptRepoInterface;
+use App\Repositories\Interfaces\ExamRepoInterface;
+use App\Repositories\Interfaces\StudentRepoInterface;
 use App\Services\Interfaces\ExpertSystemServiceInterface;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class ExpertSystemService implements ExpertSystemServiceInterface
 {
-    public function analyze(array $weakConcepts, array $prerequisites): array
+    public function __construct(protected ExamRepoInterface $examRepo, protected StudentRepoInterface $studentRepo, protected ConceptRepoInterface $conceptRepo) {}
+
+    private function analyze(array $weakConcepts, array $prerequisites): array
     {
         $process = new Process([
             'python3',
@@ -26,4 +31,19 @@ class ExpertSystemService implements ExpertSystemServiceInterface
 
         return json_decode($process->getOutput(), true);
     }
+
+
+    public function getRequires(int $examId, int $studentId): void
+    {
+        $weakConcepts = $this->studentRepo->getWeakConcepts($studentId, $examId);
+
+        $exam = $this->examRepo->findExam($examId);
+
+        $concepts = $this->conceptRepo->getConceptsBySubjectId($exam->subject_id);
+
+        $prerequisites = $this->conceptRepo->getPrerequisitesForConcepts($concepts);
+
+        $this->analyze($weakConcepts, $prerequisites);
+    }
+
 }
--- app/Services/Interfaces/ExpertSystemServiceInterface.php
+++ app/Services/Interfaces/ExpertSystemServiceInterface.php
@@ -5,5 +5,5 @@ namespace App\Services\Interfaces;
 
 interface ExpertSystemServiceInterface
 {
-    public function analyze(array $weakConcepts, array $prerequisites): array;
+    public function getRequires(int $examId, int $studentId): void;
 }
