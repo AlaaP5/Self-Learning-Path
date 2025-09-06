79a17d43c42c587f81436491da2ae3613a51e73d
AlaaP5 <alaaalshareef3012002@gmail.com>
2025-09-06 14:05:01 +0300

    add dispatch to job in storeAnswers function

--- app/Services/Classes/StudentAnswerService.php
+++ app/Services/Classes/StudentAnswerService.php
@@ -2,25 +2,28 @@
 
 namespace App\Services\Classes;
 
+use App\Jobs\AnalyzeAnswersJob;
 use App\Models\Question;
+use App\Repositories\Interfaces\ExamRepoInterface;
 use App\Repositories\Interfaces\StudentAnswerRepoInterface;
 use App\Responses\ApiResponse;
+use App\Services\Interfaces\ExpertSystemServiceInterface;
 use App\Services\Interfaces\StudentAnswerServiceInterface;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 
 class StudentAnswerService implements StudentAnswerServiceInterface
 {
-    public function __construct(protected StudentAnswerRepoInterface $studentAnswerRepo) {}
+    public function __construct(protected StudentAnswerRepoInterface $studentAnswerRepo, protected ExamRepoInterface $examRepo, protected ExpertSystemServiceInterface $expertSystemService) {}
 
     public function storeAnswers(array $data): ApiResponse
     {
-        
+
         try {
 
             $studentId = Auth::id();
             $examId = $data['exam_id'];
-            
+
             $insertData = [];
 
             foreach ($data['answers'] as $answer) {
@@ -39,7 +42,11 @@ class StudentAnswerService implements StudentAnswerServiceInterface
                 ];
             }
 
+            $this->examRepo->updateStatus($examId);
+
             $this->studentAnswerRepo->saveAnswer($insertData);
+
+            dispatch(new AnalyzeAnswersJob($this->expertSystemService, $examId, $studentId));
 
             return ApiResponse::success(null, __('shared.success'));
         } catch (Exception $e) {
