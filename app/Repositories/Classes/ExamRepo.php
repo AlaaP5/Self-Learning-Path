0b95fba2c604a22c3050298cb343917b4a9aca46
AlaaP5 <alaaalshareef3012002@gmail.com>
2025-09-06 14:03:34 +0300

    add updateStatus and findExam functions

--- app/Repositories/Classes/ExamRepo.php
+++ app/Repositories/Classes/ExamRepo.php
@@ -4,7 +4,7 @@ namespace App\Repositories\Classes;
 
 use App\Models\Exam;
 use App\Repositories\Interfaces\ExamRepoInterface;
-
+use Illuminate\Support\Facades\Auth;
 
 class ExamRepo implements ExamRepoInterface
 {
@@ -19,9 +19,31 @@ class ExamRepo implements ExamRepoInterface
 
     public function getQuesOfExam(int $examId)
     {
-        $exam= $this->examModel->where('id', $examId)->first();
+        $exam = $this->examModel->where('id', $examId)->first();
         return $questions = $exam?->questions;
     }
 
+    public function getExams()
+    {
+        $gradeId = Auth::user()->grade_id;
 
+        return $this->examModel
+            ->join('subjects', 'exams.subject_id', '=', 'subjects.id')
+            ->where('subjects.grade_id', $gradeId)
+            ->select('exams.*', 'subjects.name as subject_name')
+            ->get();
+    }
+
+    public function updateStatus(int $examId): void
+    {
+        $this->examModel->where('id', $examId)
+            ->update([
+                'is_active' => false
+            ]);
+    }
+
+    public function findExam(int $examId): Exam
+    {
+        return $this->examModel->where('id', $examId)->first();
+    }
 }
--- app/Repositories/Interfaces/ExamRepoInterface.php
+++ app/Repositories/Interfaces/ExamRepoInterface.php
@@ -3,8 +3,13 @@
 
 namespace App\Repositories\Interfaces;
 
+use App\Models\Exam;
+
 interface ExamRepoInterface
 {
     public function findActiveExam(int $examId);
     public function getQuesOfExam(int $examId);
+    public function getExams();
+    public function updateStatus(int $examId): void;
+    public function findExam(int $examId): Exam;
 }
